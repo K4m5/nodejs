32cdf83e2b25b6ac9db4d2b76b9f740d57737ab0
K4m5 <doquan15012002@gmail.com>
2025-01-05 01:30:14 +0700

    nodejs sever

---
+++ app/config/vnpay.js
@@ -0,0 +1,6 @@
+module.exports = {
+    vnp_TmnCode: 'SL2XZSC6', // Mã TMN do VNPay cung cấp
+    vnp_HashSecret: 'DA5QTHQE9HONJIPZ5P8N6VFZEB25I10S', // Mã bí mật
+    vnp_Url: 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html', // URL thanh toán
+    vnp_ReturnUrl: 'http://localhost:5173/checkout', // URL trả về sau khi thanh toán
+};
